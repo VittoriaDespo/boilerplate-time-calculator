b16020d194971fe9d31d7a19d1e793deb9c6a322
VittoriaDespo <105251001+VittoriaDespo@users.noreply.github.com>
2023-06-23 18:51:02 +0000

    Adding period

--- time_calculator.py
+++ time_calculator.py
@@ -19,13 +19,17 @@ def add_time(start, duration, ):
   # Formatting minutes
   if minutes < 10: 
     minutes = f'0{minutes}'
+  if period == "PM" and hour >= 12:
+    period = "AM"
+  elif period == "AM" and hour >= 12:
+    period = "PM"
   # Attribute the result to a new var
-  new_time = f'{hour}:{minutes}'
+  new_time = f'{hour}:{minutes} {period}'
   # Getting the hour with one decimal
   hours = {13:1,14:2,15:3,16:4,17:5,18:6,19:7,20:8,21:9,22:10,23:11,24:12}
   if hour in hours:
     final_hour = hours.get(hour)
-    new_time = f'{final_hour}:{minutes}'
+    new_time = f'{final_hour}:{minutes} {period}'
     return new_time
   else:
     return new_time
