dc18cf8b45144e6a76cfc4fb5f51b4527b0d0e54
VittoriaDespo <105251001+VittoriaDespo@users.noreply.github.com>
2023-07-25 01:22:16 +0000

    changing logic

--- time_calculator.py
+++ time_calculator.py
@@ -1,38 +1,51 @@
-def add_time(start, duration, ):
-  print(start,duration)
-  time,period = start.split()
-  # Converting string in float
-  float_time = float(time.replace(':','.'))
-  float_duration = (float(duration.replace(':','.')))
-  # Calculating duration's mod
-  mod_duration = float_duration%24
-  # Add duration's mod to the time
-  sum = round(float_time + mod_duration,2)
-  # formatting the var sum to get a sum with 2 decimal
-  sum = f'{sum:.2f}'
-  # Converting into integer and split the result
-  hour,minutes = map(int,sum.split('.'))
-  # Condition to add minutes to hour
-  if minutes >=60:
-    hour += 1
-    minutes = minutes-60
-  # Formatting minutes
-  if minutes < 10: 
-    minutes = f'0{minutes}'
-  if period == "PM" and hour >= 12:
+def add_time(start, duration, day = None):
+
+  time, period = start.split()
+  hour, minutes = map(int,time.split(':'))
+  duration_hour, duration_minutes = map(int,duration.split(':'))
+  print(hour, minutes)
+  print(duration_hour, duration_minutes)
+
+  
+  if period == 'PM':
+    hour = hour + 12
+    
+  sum_hour = hour + duration_hour
+  
+  minutes_hour = sum_hour * 60
+  
+  min_tot = minutes+duration_minutes
+ 
+  final_hour = minutes_hour/60
+  if min_tot >=60:
+    final_hour += 1
+    min_tot = min_tot-60
+  if min_tot < 10:
+    min_tot = f'0{min_tot}'
+  day,final_hour = divmod(int(final_hour),24)
+  if final_hour < 12:
     period = "AM"
-  elif period == "AM" and hour >= 12:
-    period = "PM"
-  # Attribute the result to a new var
-  new_time = f'{hour}:{minutes} {period}'
-  # Getting the hour with one decimal
-  hours = {13:1,14:2,15:3,16:4,17:5,18:6,19:7,20:8,21:9,22:10,23:11,24:12}
-  if hour in hours:
-    final_hour = hours.get(hour)
-    new_time = f'{final_hour}:{minutes} {period}'
-    return new_time
   else:
-    return new_time
+    period = "PM"
+    final_hour = final_hour - 12
+  if final_hour == 0:
+    final_hour = 12
+  print(final_hour)
+
+
+
+
+
+
+    
+  if day == 1:
+      next_day = '(next day)'
+      return (f'{final_hour}:{min_tot} {period} {next_day}')
+  elif day > 1:
+      next_day = f'({day} days later)'
+      return (f'{final_hour}:{min_tot} {period} {next_day}')
+  else:
+      return (f'{final_hour}:{min_tot} {period}')
     
   
  
